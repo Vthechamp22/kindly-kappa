88ac812b8b5b64661a260f7f6ba54e6cd9529bfe
Luke Hankey <lukehankey@gmail.com>
2022-07-22 17:27:00 +0100

    feat: Adds two simple code modifiers
    * remove_indentation and remove_end_colon

---
+++ modifiers.py
@@ -0,0 +1,56 @@
+from typing_extensions import Self
+
+FOUR_SPACES = "    "
+TWO_SPACES = "  "
+
+
+class Modifiers:
+    """This class has different functions which introduce different types of bugs.
+
+    All the functions should return Self so they can be chained to get the final
+    output. The number of chained functions is determined by the difficulty.
+    """
+
+    def __init__(self, file_contents: list[str]) -> None:
+        self.file_contents = file_contents
+        self.modified_contents = file_contents
+
+    @property
+    def output(self) -> list[str]:
+        """Returns the modified code, if any modifications have been done."""
+        return self.modified_contents
+
+    def remove_indentation(self) -> Self:
+        """A code modifier that reduces indentation from four spaces to two spaces."""
+        line_numbers = []
+        for num, line in enumerate(self.file_contents):
+            if line.startswith(FOUR_SPACES):
+                line_numbers.append(num)
+
+        for num in line_numbers:
+            self.modified_contents[num] = self.file_contents[num].replace(FOUR_SPACES, TWO_SPACES)
+
+        return self
+
+    def remove_end_colon(self) -> Self:
+        """A code modifier that causes a SyntaxError.
+
+        This will remove the colon after a function definition, loop, or if statement.
+        """
+        line_numbers = []
+        for num, line in enumerate(self.file_contents):
+            if line.endswith(":\n"):
+                line_numbers.append(num)
+
+        for num in line_numbers:
+            self.modified_contents[num] = self.file_contents[num].replace(":", "")
+
+        return self
+
+
+if __name__ == "__main__":
+    test_lines = ["def say_hello() -> str:\n", '    return "Hello!"\n', "\n"]
+
+    modifiers = Modifiers(test_lines)
+    new_lines = modifiers.remove_indentation().remove_end_colon()
+    print(new_lines.output)
