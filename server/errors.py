15011a353ff596fb18b22d4b37f4f28b2e929ab8
Luke Hankey <lukehankey@gmail.com>
2022-07-24 23:26:34 +0100

    Add RoomNotFoundError

--- server/errors.py
+++ server/errors.py
@@ -5,3 +5,15 @@ class KappaCloseCodes(IntEnum):
     """Custom error codes for closing websocket connections."""
 
     InvalidError = 4000
+
+    RoomNotFound = 4001
+
+
+class RoomNotFoundError(Exception):
+    """Custom exception raised when joining a room with an invalid code."""
+
+    def __init__(self, message: str, code: KappaCloseCodes) -> None:
+        super().__init__(message)
+
+        self.code = code.value
+        self.message = message
