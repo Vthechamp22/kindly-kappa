5f29cc2f96dc44397b6f02c2bc77b2d4e7b8b5b1
Luke Hankey <lukehankey@gmail.com>
2022-07-24 20:12:33 +0100

    fix: Update type of error code

--- server/errors.py
+++ server/errors.py
@@ -10,7 +10,7 @@ class KappaCloseCodes(IntEnum):
 class InvalidCodeError(Exception):
     """Custom exception raised when joining a room with an invalid code."""
 
-    def __init__(self, message: str, code: str) -> None:
+    def __init__(self, message: str, code: KappaCloseCodes) -> None:
         super().__init__(message)
 
         self.code = code.value
