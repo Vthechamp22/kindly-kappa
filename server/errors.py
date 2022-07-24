f0dc31807e7da940b5dd41a147fa0a22c09c73b2
Luke Hankey <lukehankey@gmail.com>
2022-07-24 21:40:09 +0100

    Removed custom error. No longer needed

--- server/errors.py
+++ server/errors.py
@@ -5,13 +5,3 @@ class KappaCloseCodes(IntEnum):
     """Custom error codes for closing websocket connections."""
 
     InvalidError = 4000
-
-
-class InvalidCodeError(Exception):
-    """Custom exception raised when joining a room with an invalid code."""
-
-    def __init__(self, message: str, code: KappaCloseCodes) -> None:
-        super().__init__(message)
-
-        self.code = code.value
-        self.message = message
