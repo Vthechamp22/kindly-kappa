f09edcce966a7e519d6d886430765a51a419baee
Kappa <72104527+kappq@users.noreply.github.com>
2022-07-26 00:51:18 +0200

    Add custom status codes

--- server/errors.py
+++ server/errors.py
@@ -1,19 +1,19 @@
 from enum import IntEnum
 
 
-class KappaCloseCodes(IntEnum):
-    """Custom error codes for closing websocket connections."""
+class StatusCode(IntEnum):
+    """Custom status codes for a WebSocket event."""
 
-    InvalidError = 4000
-
-    RoomNotFound = 4001
+    SUCCESS = 200
+    INVALID_ERROR = 4000
+    ROOM_NOT_FOUND = 4001
 
 
 class RoomNotFoundError(Exception):
     """Custom exception raised when joining a room with an invalid code."""
 
-    def __init__(self, message: str, code: KappaCloseCodes) -> None:
+    def __init__(self, message: str) -> None:
         super().__init__(message)
 
-        self.code = code.value
+        self.code = StatusCode.ROOM_NOT_FOUND
         self.message = message
