cf2a8c98a8d6cbebbc7993aa21d36fda8aaf3a0f
Vivaan <parashar.xtra@gmail.com>
2022-07-25 19:46:38 +0530

    forgot to remove fontawesome imports

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,22 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
-/* import the fontawesome core */
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-// import specific icons
-import {
-  faArrowRightFromBracket,
-  faXmark,
-  faSpinner,
-} from "@fortawesome/free-solid-svg-icons";
-
-/* add icons to the library */
-library.add(faArrowRightFromBracket, faXmark, faSpinner);
-
 import "./assets/main.css";
 
-createApp(App).component("fa-icon", FontAwesomeIcon).mount("#app");
+createApp(App).mount("#app");
