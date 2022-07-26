f3aa2827e8a95a31b583149e5fcf890d6bdd2932
Luke Hankey <lukehankey@gmail.com>
2022-07-27 00:41:13 +0100

    test: Fix tests for output

--- tests/test_modifiers.py
+++ tests/test_modifiers.py
@@ -1,5 +1,6 @@
 import pytest
 
+from server.events import ReplaceData
 from server.modifiers import FOUR_SPACES, STATEMENTS, TYPES, Modifiers
 
 test_input = [
@@ -123,7 +124,8 @@ class TestModifiers:
     def test_modified_output(self, create_instance: Modifiers, difficulty: int):
         create_instance.difficulty = difficulty
         value = create_instance.output
+        print(value)
 
         assert create_instance.difficulty == difficulty
-        assert isinstance(value, list)
+        assert isinstance(value, ReplaceData)
         assert difficulty <= create_instance.modified_count
