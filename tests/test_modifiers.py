2623a83c3ccc5b4bfb50c6172006ce1cc865e926
Montarex23 <montarex23@gmail.com>
2022-07-26 15:59:14 +0200

    Fixed issues with previous commit

--- tests/test_modifiers.py
+++ tests/test_modifiers.py
@@ -4,7 +4,7 @@ from server.modifiers import FOUR_SPACES, STATEMENTS, TYPES, Modifiers
 
 test_input = [
     "def say_hello() -> str:\n",
-    '    if 1 == True and 0 == False: return "Hello!"\n',
+    '    return "Hello!"\n',
     "say_hello()\n",
     "\n",
 ]
@@ -27,6 +27,12 @@ def create_instance_with_property():
     yield Modifiers(property_input + test_input)
 
 
+@pytest.fixture
+def create_instance_with_boolean():
+    boolean_input = ["    if 1 == True and 0 == False:\n", "        return 'Hello!'\n"]
+    yield Modifiers(test_input[:1] + boolean_input + test_input[2:])
+
+
 class TestModifiers:
     def test_removing_indentation(self, create_instance: Modifiers):
         value = create_instance.remove_indentation()
@@ -35,7 +41,7 @@ class TestModifiers:
         assert value.file_contents[1].startswith(FOUR_SPACES)
         assert value.modified_contents == [
             "def say_hello() -> str:\n",
-            '  if 1 == True and 0 == False: return "Hello!"\n',
+            '  return "Hello!"\n',
             "say_hello()\n",
         ]
 
@@ -82,25 +88,25 @@ class TestModifiers:
         assert isinstance(value, Modifiers)
         assert any(stmt in modified for stmt in STATEMENTS for modified in value.modified_contents)
 
-    def test_reversing_booleans(self, create_instance: Modifiers):
-        value = create_instance.reverse_booleans()
+    def test_reversing_booleans(self, create_instance_with_boolean: Modifiers):
+        value = create_instance_with_boolean.reverse_booleans()
 
         assert isinstance(value, Modifiers)
         assert ("True" not in value.modified_contents[1]) != ("False" not in value.modified_contents[1])
         assert (value.modified_contents[1].count("True") == 2) != (value.modified_contents[1].count("False") == 2)
 
-    def test_breaking_equals_statement(self, create_instance: Modifiers):
-        value = create_instance.break_equals_statement()
+    def test_breaking_equals_statement(self, create_instance_with_boolean: Modifiers):
+        value = create_instance_with_boolean.break_equals_statement()
 
         assert isinstance(value, Modifiers)
         assert "==" not in value.modified_contents[1]
-        assert " = " in value.modified_contents[1]
+        assert "=" in value.modified_contents[1]
 
     def test_mixing_type_keywords(self, create_instance: Modifiers):
         value = create_instance.mix_type_keywords()
 
         assert isinstance(value, Modifiers)
-        assert "str" not in value.modified_contents
+        assert "str" not in value.modified_contents[0]
         assert any(value_type in value.modified_contents[0] for value_type in TYPES)
 
     def test_adding_or_removing_brackets(self, create_instance: Modifiers):
