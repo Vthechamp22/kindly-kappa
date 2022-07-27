51137a9aa92fee176d9126b335a2cd811384f60a
Luke Hankey <lukehankey@gmail.com>
2022-07-27 23:47:13 +0100

    Remove whitespace from StatusCodes

--- server/codes.py
+++ server/codes.py
@@ -5,11 +5,7 @@ class StatusCode(IntEnum):
     """Custom status codes for a WebSocket event."""
 
     SUCCESS = 4000
-
     ROOM_NOT_FOUND = 4001
-
     INVALID_REQUEST_DATA = 4002
-
     DATA_NOT_FOUND = 4003
-
     ROOM_ALREADY_EXISTS = 4004
