e3f6ddc883a485eb1c0ad877d6930af8b3af7f15
Evorage <68397475+Evorage0@users.noreply.github.com>
2022-07-30 11:36:39 +0100

    Updated frontend 🥳

--- frontend/src/components/Room.vue
+++ frontend/src/components/Room.vue
@@ -1,104 +1,177 @@
 <script setup lang="ts">
 import * as monaco from "monaco-editor";
-import { onMounted } from "vue";
+import { onMounted, ref, toRaw } from "vue";
 import { onedark } from "../assets/js/theme";
 
-const emit = defineEmits(["join"]);
+const props = defineProps(["state", "sync"]);
+const emit = defineEmits(["leaveRoom"]);
+
+let collaborators = ref(toRaw(props.sync.collaborators));
+let code = props.sync.code;
+let editor;
 
 onMounted(() => {
-  monaco.editor.defineTheme("OneDarkPro", onedark);
-  monaco.editor.setTheme("OneDarkPro");
+    monaco.editor.defineTheme("OneDarkPro", onedark);
+    monaco.editor.setTheme("OneDarkPro");
+    editor = monaco.editor.create(document.getElementById("container"), {
+        language: "python",
+    });
+    editor.getModel().onDidChangeContent(contentHandler);
+})
 
-  var e = monaco.editor.create(document.getElementById("content"), {
-    value: "",
-    language: "python",
-    insertSpaces: true,
-    theme: "OneDarkPro",
-  });
+function positionToIndex(line, col) {
+    let index = 0;
+    for (let i = 0; i < line.length; i++) {
+        index += editor.getLineLength(i) + 2;
+    }
+    return index + col - 1;
+}
 
-  window.setEditor(e);
+function contentHandler(ev) {
+    if (code === editor.getModel().getValue()) return;
 
-  e.getModel()?.onDidChangeContent(window.handleContentChange);
-});
+    const changes = ev.changes.map(change => {
+        return {
+            from: positionToIndex(
+                change.range.startLineNumber,
+                change.range.startColumn
+            ),
+            to: positionToIndex(
+                change.range.endLineNumber,
+                change.range.endColumn
+            ),
+            value: change.text
+        }
+    });
+
+    props.state.websocket.send(JSON.stringify({
+        type: "replace",
+        data: {
+            code: changes
+        }
+    }));
+
+    code = editor.getModel().getValue();
+}
+
+props.state.websocket.onmessage = function (ev) {
+    const message = ev.data;
+
+    switch (message.type) {
+        case "connect":
+            collaborators.push(message.data);
+            break;
+
+        case "disconnect":
+            collaborators = collaborators.filter(c => {
+                c.id !== message.data.id;
+            });
+            break;
+
+        case "replace":
+            message.data.code.forEach(change => {
+                code = code.substring(0, change.from) + change.value + code.substring(change.to);
+            });
+    }
+}
 
 function leaveRoom() {
-  monaco.editor.getModels().forEach((model) => model.dispose());
-  emit("join", "leave");
+    props.state.websocket.send(JSON.stringify({
+        "type": "disconnect",
+        "data": {}
+    }));
+    emit("leaveRoom");
 }
 </script>
 
 <template>
-  <div id="room">
-    <div id="sidebar">
-      <h2 class="text-6xl text-white m-3">Collaborators</h2>
-      <ul id="collabul"></ul>
-      <button class="btn btn-primary mt-auto" @click="leaveRoom()">
-        <fa-icon icon="fa-solid fa-arrow-right-from-bracket" />
-        Leave Room
-      </button>
-    </div>
+    <div id="room">
+        <div id="sidebar">
+            <h2 class="text-6xl text-white m-3">Collaborators</h2>
+            <ul style="margin-left:20px">
+                <li v-for="collaborator in collaborators">{{ collaborator.username }}</li>
+            </ul>
+            <ul id="collabul"></ul>
+            <button class="btn btn-primary mt-auto" @click="leaveRoom">
+                <fa-icon icon="fa-solid fa-arrow-right-from-bracket" />
+                Leave Room
+            </button>
+        </div>
 
-    <div id="content"></div>
-  </div>
+        <div id="content">
+            <div id="container">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
 #room {
-  height: 100%;
-  display: grid;
-  grid-template-columns: 1fr 3fr;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
 }
 
 #content {
-  text-align: left;
+    text-align: left;
 }
 
 #sidebar,
 #content {
-  border: solid white;
+    border: solid white;
 }
 
 #sidebar {
-  border-width: 4px 2px 4px 4px;
+    border-width: 4px 2px 4px 4px;
 }
 
 #content {
-  border-width: 4px 4px 4px 2px;
+    border-width: 4px 4px 4px 2px;
+    padding: 4px;
 }
 
 #sidebar h1 {
-  text-align: center;
+    text-align: center;
 }
 
 ul {
-  text-align: center;
-  font-size: 2em;
+    text-align: center;
+    font-size: 2em;
 }
 
 li {
-  margin: 30px;
+    margin: 30px;
 }
 
 #sidebar,
 #content {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
 li {
-  text-align: left;
-  color: white;
-  font-size: 24px;
-  margin-left: 48px;
-  list-style: disc;
+    text-align: left;
+    color: white;
+    font-size: 24px;
+    margin-left: 48px;
+    list-style: disc;
 }
 
 .fa-arrow-right-from-bracket {
-  -webkit-transform: scale(-1, 1);
-  -moz-transform: scale(-1, 1);
-  -ms-transform: scale(-1, 1);
-  -o-transform: scale(-1, 1);
-  transform: scale(-1, 1);
-  margin-right: 1em;
+    -webkit-transform: scale(-1, 1);
+    -moz-transform: scale(-1, 1);
+    -ms-transform: scale(-1, 1);
+    -o-transform: scale(-1, 1);
+    transform: scale(-1, 1);
+    margin-right: 1em;
+}
+
+#container {
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
 }
 </style>
